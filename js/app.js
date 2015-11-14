951920432f589f15ffd0c38100c5d05a4c47d381
huijie guan <guanh3@uw.edu>
2015-11-14 11:37:18 -0800

    js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 'use strict'		
-angular.module('SignUpApp', [])
-.controller('MyCtrl', ['$scope', '$http', function($scope, $http) {
-	$scope.email = {
-        text: 'me@example.com'
-      };
-		function validatePassword(){
-		  var password = document.getElementById("password")
-		  var confirm = document.getElementById("confirm");
-		  if (password.value != confirm_password.value) {
-		  	confirm.setCustomValidity("passwords don't match");
-		  } else {
-		  	confirm.setCustomValidity('');
-		  }
-		}
 
-}])
+
+angular.module('myApp', [])
+  .controller('MyCtrl', ['$scope', '$http', function($scope, $http) {
+
+	$scope.validatePassword = function(){
+	  if (password.value != confirm_password.value) {
+	  	alert("passwords don't match");
+	  } 
+	};
+}
+
+
