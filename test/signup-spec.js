bcab3d5624a5066c1baaa50d1b00cd730275e78b
yiying song <Yiying>
2015-11-16 17:39:33 -0800

    sophie finished

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,65 +1,70 @@
 'use strict';
 
-
+// describes a series of test for the sign up form
 describe('SignUp Web', function(){ 
     beforeEach(function() {
       //reload the page before each test
       browser.get('http://localhost:8000');
     });   
 
+    // test if the page displays invalid email message if the email is invalid
     it("should show that email is invalid message if the e-mail is not in corret form", function() {
-        var email = element(by.model("email"));
+        // set the email user put in to email and error message to invalidEmail 
+        var email = element(by.model("email.text"));
+        var invalidEmail = element(by.model('invalidEmail'));
+        // type "@gmail.com" and "sophie" into email and test if it meets expectations
         email.sendKeys('@gmail.com');
-        expect((email, 'ng-invalid').toEqual(true));
+        expect(invalidEmail.isDisplayed() ).toEqual(true);        
         email.sendKeys('sophie');
-        expect((email, 'ng-invalid').toEqual(true));
+        expect(invalidEmail.isDisplayed() ).toEqual(true);
     });
 
+    // test if the page displays email nedded massage if the email is touchd but left empty
     it("should show that email is required message if the email field is touched but left blank", function() {
-        var email = element(by.model('email'));
+        // set the email user put in to email and error message to noEmail
+        var email = element(by.model("email.text"));
+        var noEmail = element(by.model('noEmail'));
+        // type "email" to eamil field and clear it, then test if it meets the expectation
+        email.sendKeys('email');
+        email.clear();
+        expect(noEmail.isDisplayed() ).toEqual(true);
         email.sendKeys('');
-        expect((email, 'ng-invalid').toEqual(true));
+        expect(noEmail.isDisplayed() ).toEqual(true);
     });
 
+    // test if the error messages are hidden if the email is valid
     it("should hide error messages if the email is valid", function() {
-        var email = element(by.model('email'));
+        // set the email user put in to email and error message to invalidEmail and noEmail
+        var email = element(by.model("email.text"));
+        var invalidEmail = element(by.model('invalidEmail'));
+        var noEmail = element(by.model('noEmail'));
+        // type in a valid email to test if it meets the expectation
         email.sendKeys('ys46@uw.edu');
-        expect((email, 'ng-invalid').toEqual(false));
+        expect(invalidEmail.isDisplayed() ).toEqual(false);
+        expect(noEmail.isDisplayed() ).toEqual(false);
     });
 
+    // test if the page displays last name required message if the last name is touched but left blank
     it("should show last name is required message if the field is touched but left blank", function() {
-        var lastName = element(by.model('lastName'));
-        lastName.sendKeys('');
-        expect((lastName, 'ng-invalid').toEqual(true));
-    });
-
-    it("should hide error message if there is content in the field", function() {
+        // set lastName to the last name user put in and set the error message to noLastName
         var lastName = element(by.model("lastName"));
+        var noLastName = element(by.model("noLastName"))
+        // type in a name and clear it, and then test if it meets the expectation
         lastName.sendKeys('song');
-        expect((lastName, 'ng-invalid').toEqual(false));
-    });    
-
-    it('should be invalid when password is empty', function(){
-        var password = element(by.id('password'));
-        password.sendKeys('');
-        expect(password).getAttribute('class')).toMatch('ng-invalid');
+        lastName.clear();
+        expect(noLastName.isDisplayed() ).toEqual(true);
+        lastName.sendKeys('');
+        expect(noLastName.isDisplayed() ).toEqual(true);
     });
 
-    it('should be invalid when passwords are not matching', function(){
-        var password = element(by.id('password'));
-        var confirmPassword = element(by.id('confirmPassword'));
-        password.sendKeys('123');
-        confirmPassword.sendKeys('1234');
-        expect(password).getAttribute('class')).toMatch('ng-invalid');
-    });
-
-    it('should be valid when passwords are matching', function(){
-        var password = element(by.id('password'));
-        var confirmPassword = element(by.id('confirmPassword'));
-        password.sendKeys('123');
-        confirmPassword.sendKeys('123');
-        expect(password).getAttribute('class')).toMatch('ng-valid');
-    });
+    // test if the error message is hidden if the last name is filled
+    it("should hide error message if there is content in the field", function() {
+        // set lastName to the last name user put in and set the error message to noLastName
+        var lastName = element(by.model("lastName"));
+        var noLastName = element(by.model("noLastName"))
+        // type in a name and then test if it meets with the expectations
+        lastName.sendKeys('song');
+        expect(noLastName.isDisplayed() ).toEqual(false);
+    });   
 
 })
-
